f6f40cedfd00786136a14b18907b421ffce46fb6
venugopalsgnew <venugopal.aix@gmail.com>
2020-12-07 14:53:31 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  access_key =  var.accesskey
+  secret_key =  var.secretkey
+}
+
+resource "aws_instance" "web2" {
+  ami           = var.image
+  instance_type = var.instancetype
+
+  tags = {
+    Name = "HelloWorld3"
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "estin-hassi-module-27nov202012"
+    key            = "global/nonprod/terraform.tfstate"
+    region         = "ap-south-1" 
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "image" {
+    type    = string
+    default = "ami-08f63db601b82ff5f" 
+}
+variable "instancetype" {
+    type    = string
+    default = "t2.micro"
+}
+
+variable "accesskey" {
+    type = string
+    
+}
+
+variable "secretkey" {
+    type = string
+}
